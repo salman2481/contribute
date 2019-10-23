e1bcdef07dd4777f4a38a1489c9de0f279f2497e
salman2481 <55204448+salman2481@users.noreply.github.com>
2019-10-23 07:18:12 -0700

    start 1

--- start.cpp
+++ start.cpp
@@ -1,12 +1,20 @@
+//This program does the basic arithmetic operation
+//i.e. addition and subtraction
 #include <iostream>
 using namespace std;
 int main()
 {
-    int a, b, sum;
+    int a, b, sum, sub;
     cout << "Enter the two numbers you want to add: ";
     cin >> a >> b;
     sum = (a + b);
     cout << "The addition of " << a << " and " << b << "are: " << sum;
+    system ("cls"),
+    cout << "Enter the two numbers you want to subtract: ";
+    cin >> a >> b;
+    sub = (a - b);
+    cout << "The subtraction of " << a << " and " << b << "are: " << sub;
 
     return 0;
 }
+#pragma once
